3976b0daa3b6ba911fec9e79870fa6f89bfc1e87
VelkeiMiklos <velkei18@gmail.com>
2018-04-17 17:40:37 +0200

    Initial Commit

---
+++ drone/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  drone
+//
+//  Created by Velkei Miklós on 2018. 04. 17..
+//  Copyright © 2018. NeonatCore. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
